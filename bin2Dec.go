c5fe0fa53cd66838181a51187a3afe058fc11998
netrunner2 <aodeyale@choosefreedomit.com>
2024-01-25 18:23:55 -0500

    added feature to get user input and check if user entered a non binary num

--- bin2Dec.go
+++ bin2Dec.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"strconv"
+	"regexp"
 )
 
 func main() {
@@ -13,17 +15,26 @@ func main() {
 	var userInput int
 	fmt.Print("Enter a binary number: ")
 	fmt.Scan(&userInput)
-	userInputCopy := userInput
 
-	index := 0
-	decimalNum := 0
-	// convert user input into decimal
-	for userInput != 0 {
-		remainder := userInput % 10
-		userInput = userInput / 10
-		decimalNum = decimalNum + remainder * int(math.Pow(2, float64(index)))
-		index++
+	// convert input to string to check if it contains anything other than 1 or 0
+	inputString := strconv.Itoa(userInput)
+	notValidInput, _ := regexp.MatchString("[2-9]", inputString)
+
+	if notValidInput {
+		fmt.Println("You can only enter combonations of 0 or 1 for a binary num")
+	} else {
+		userInputCopy := userInput
+
+		index := 0
+		decimalNum := 0
+		// convert user input into decimal
+		for userInput != 0 {
+			remainder := userInput % 10
+			userInput = userInput / 10
+			decimalNum = decimalNum + remainder * int(math.Pow(2, float64(index)))
+			index++
+		}
+
+		fmt.Printf("Binary num %v converted to decimal %v\n", userInputCopy, decimalNum)
 	}
-
-	fmt.Printf("Binary num %v converted to decimal %v\n", userInputCopy, decimalNum)
 }
